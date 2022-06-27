3c00c7513306d5175412fe3e1b11d3093f9131bb
Juliana <juliana@Julianas-MacBook-Pro.local>
2022-06-27 13:33:31 +0200

    HomePage fetch function deleted

--- src/page/home-page/home-page.ts
+++ src/page/home-page/home-page.ts
@@ -12,42 +12,18 @@ import styles from './home-page.scss';
 export default class HomePage extends PageElement {
   static styles = [ css([styles] as unknown as TemplateStringsArray) ];
 
-  @property({type: Array})
-  characters = [];
-
 
   constructor() {
     super({title: 'Home'});
-  }
-
-  protected updated(_changedProperties: Map<string | number | symbol, unknown>): void {
-    console.log(this.characters);
   }
 
   render() {
     return html`<div>
     
     <h2>${localize('HomePage')}</h2>
-
-    <button @click=${this.clickHandler}>
-    ${this.characters.length === 0 ? 'Get the characters' : 'Hide the characters' }
-    </button>
-
-    ${this.characters.map((item: {name: string}) => html`<p>${item.name}</p>`)}
     
     </div>`;
   }
-
-
-  private clickHandler(e: MouseEvent) {
-    console.log(e);
-    if (this.characters.length === 0) {
-      fetch('http://hp-api.herokuapp.com/api/characters').then( resp => resp.json() ).then( data => this.characters = data );
-    } else {
-      this.characters = [];
-    }
-  }
-
 
 
 }
