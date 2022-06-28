814ffb228f444cd6c531c5b05c50e7125a7ad0b7
Juliana <juliana@Julianas-MacBook-Pro.local>
2022-06-28 15:32:50 +0200

    Remove paddings and margins from templates

--- src/app-root/styles.ts
+++ src/app-root/styles.ts
@@ -3,8 +3,6 @@ import {css} from 'lit';
 export const styles = css`
   :host {
     display: block;
-    padding: 20px;
-    max-width: 60%;
     margin: 0 auto;
     background: var(--color-background-primary);
     color: var(--color-foreground-primary);
--- src/component/router/router-element/styles.ts
+++ src/component/router/router-element/styles.ts
@@ -5,5 +5,7 @@ export const styles = css`
     display: block;
     width: 100%;
     height: 100%;
+    display: flex;
+    justify-content: center;
   }
 `;
