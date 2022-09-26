52d4d32ffd293508e3f0f1a159135632926314b5
juliana <rochajuliana@pm.me>
2022-09-26 12:48:58 +0200

    First part of the code upload

--- src/page/geo-page/geo-page.ts
+++ src/page/geo-page/geo-page.ts
@@ -57,10 +57,11 @@ export default class GeoPage extends PageElement {
         .attr('id', (d: any) => d.id);
     });
 
+    d3.tsv('world-temprature.tsv').then((data: any) => {
+      // 60 is the number of class in temperature.css
 
-      d3.tsv('world-temprature.tsv').then((data: any) => {
-        // 60 is the number of class in temperature.css
-        var quantile = d3.scaleQuantile()
+      var quantile = d3
+        .scaleQuantile()
         .domain([
           d3.min(data, function (e: any) {
             return +e.temperature;
@@ -70,103 +71,61 @@ export default class GeoPage extends PageElement {
           }),
         ])
         .range(d3.range(60));
-      })
 
-        
+      var legend = svg
+        .append('g')
+        .attr('transform', 'translate(40, 10)')
+        .attr('id', 'legend');
 
-        var legend = svg
-          .append('g')
-          .attr('transform', 'translate(40, 10)')
-          .attr('id', 'legend');
+      legend
+        .selectAll('.colorbar')
+        .data(d3.range(60))
+        .enter()
+        .append('svg:rect')
+        .attr('y', function (d) {
+          return d * 5 + 'px';
+        })
+        .attr('height', '5px')
+        .attr('width', '20px')
+        .attr('x', '0px')
+        .attr('class', function (d) {
+          return 'temperature-' + d;
+        });
 
-        legend
-          .selectAll('.colorbar')
-          .data(d3.range(60))
-          .enter()
-          .append('svg:rect')
-          .attr('y', function (d) {
-            return d * 5 + 'px';
-          })
-          .attr('height', '5px')
-          .attr('width', '20px')
-          .attr('x', '0px')
-          .attr('class', function (d) {
-            return 'temperature-' + d;
-          });
-
-        legendScale = d3.scale
-          .linear()
-          .domain([
-            d3.min(data, function (e) {
+      let legendScale = d3
+        .scaleLinear()
+        .domain([
+          Number(
+            d3.min(data, function (e: any) {
               return +e.temperature;
-            }),
-            d3.max(data, function (e) {
+            })
+          ),
+          Number(
+            d3.max(data, function (e: any) {
               return +e.temperature;
-            }),
-          ])
-          .range([0, 60 * 5]);
+            })
+          ),
+        ])
+        .range([0, 60 * 5]);
 
-        legendAxis = d3.svg
-          .axis()
-          .scale(legendScale)
-          .orient('left')
-          .tickSize(6)
-          .ticks(10);
+      let legendAxis = d3.axisLeft(legendScale);
 
-        legendLabels = svg
-          .append('g')
-          .attr('transform', 'translate(30, 10)')
-          .attr('class', 'y axis')
-          .call(legendAxis);
+      let legendLabels = svg
+        .append('g')
+        .attr('transform', 'translate(30, 10)')
+        .attr('class', 'y axis')
+        .call(legendAxis);
 
-        var temperatures = [];
-        data.forEach(function (e, i) {
+      let temperatures = [];
+      data.forEach(
+        (e: {country: string; temperature: string | number}, i: any) => {
           temperatures.push({id: e.country, temperature: e.temperature});
           d3.select('#' + e.country).attr('class', function (d) {
             return 'country temperature-' + quantile(+e.temperature);
           });
-        });
-
-        countries.on('mouseover', function (d) {
-          var temperature = 0;
-          for (var i = 0; i < temperatures.length; ++i) {
-            if (temperatures[i].id === d.id) {
-              temperature = temperatures[i].temperature;
-            }
-          }
-          d3.select('#rightbox').html(
-            '<div class="box-title"><h3>Température moyenne annuelle</h3></div>' +
-              'Pays : <span style="font-weight:bold;">' +
-              d.properties.name +
-              '</span><br/>Température : <span style="font-weight:bold;">' +
-              temperature +
-              '°'
-          );
-        });
-      });
+        }
+      );
     });
-
-    var λ = d3.scale.linear().domain([0, width]).range([-180, 180]);
-
-    var φ = d3.scale.linear().domain([0, height]).range([90, -90]);
-
-    var drag = d3.behavior
-      .drag()
-      .origin(function () {
-        var r = projection.rotate();
-        return {
-          x: λ.invert(r[0]),
-          y: φ.invert(r[1]),
-        };
-      })
-      .on('drag', function () {
-        projection.rotate([λ(d3.event.x), φ(d3.event.y)]);
-        svg.selectAll('.country').attr('d', path);
-
-        svg.selectAll('.graticule').datum(graticule).attr('d', path);
-      });
-
-    svg.call(drag);
   }
 
   disconnectedCallback(): void {
