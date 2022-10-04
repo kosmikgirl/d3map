83e97a2d7a5c5d740d0b242be4c8759d61ba0f72
juliana <rochajuliana@pm.me>
2022-10-04 15:13:14 +0200

    Styles and information added to the home and about page

--- src/page/about-page/about-page.ts
+++ src/page/about-page/about-page.ts
@@ -2,27 +2,40 @@ import {html, PropertyValues} from 'lit';
 import {customElement, property} from 'lit/decorators.js';
 import {RouteDataParam} from '../../data/enum';
 import PageElement from '../abstract/page-element';
-import { css } from 'lit';
+import {css} from 'lit';
 import styles from './about-page.scss';
 
 @customElement('about-page')
 export default class AboutPage extends PageElement {
-  static styles = [ css([styles] as unknown as TemplateStringsArray) ];
+  static styles = [css([styles] as unknown as TemplateStringsArray)];
 
   constructor() {
     super({title: 'About'});
   }
 
   render() {
-    return html`<div>
-    
+    return html`<div class="about-container">
       <h2>
-        AboutPage ${this.routeData?.[RouteDataParam.ID] || 'no id'}
+        About me and the motivations that took me to this personal project
       </h2>
 
-      <p>About Page loaded</p>
-    
+      <p>
+        I'm
+        <a href="https://www.linkedin.com/in/rochajulianacarolina/"
+          >Juliana Rocha,</a
+        >
+        front end developer, born and raised in Argentina, working in the IT
+        industry for a while now.
+      </p>
+
+      <p>
+        The current Google project that I am working on, required a migration
+        from Vue to Lit, so we needed to learn the library. Also, d3.js is
+        included since it is a really nice library to show dynamic data in a
+        nice, neat, fun and creative way!
+      </p>
+
+      <p>I was really fun doing it, I hope you enjoy the results!</p>
     </div>`;
   }
-
 }
--- src/page/home-page/home-page.ts
+++ src/page/home-page/home-page.ts
@@ -2,30 +2,38 @@ import {html, PropertyValues} from 'lit';
 import {customElement, property, state} from 'lit/decorators.js';
 import {msg as localize, localized} from '@lit/localize/lit-localize';
 import PageElement from '../abstract/page-element';
-import { css } from 'lit';
+import {css} from 'lit';
 import styles from './home-page.scss';
-
-
 
 @localized()
 @customElement('home-page')
 export default class HomePage extends PageElement {
-  static styles = [ css([styles] as unknown as TemplateStringsArray) ];
-
+  static styles = [css([styles] as unknown as TemplateStringsArray)];
 
   constructor() {
     super({title: 'Home'});
   }
 
   render() {
-    return html`<div>
-    
-    <h2>${localize('HomePage')}</h2>
+    return html`<div class="homepage-container">
+      <h2>Personal project made with Lit.dev</h2>
 
-    <p>HomePage loaded</p>
-    
+      <p>
+        This small project is made using
+        <a href="https://lit.dev/" target="_blank">Lit.dev,</a> and
+        <a href="https://d3js.org/" target="_blank">d3.js</a> libraries.
+      </p>
+
+      <p>
+        Lit is a simple library for building fast, lightweight web components.
+        At Lit's core is a boilerplate-killing component base class that
+        provides reactive state, scoped styles, and a declarative template
+        system that's tiny, fast and expressive.
+      </p>
+
+      <p>
+        D3.js is a JavaScript library for manipulating documents based on data.
+      </p>
     </div>`;
   }
-
-
 }
